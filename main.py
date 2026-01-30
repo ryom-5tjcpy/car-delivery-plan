f4584e6ec629af9a23b9ac48b34c0a7d198b68f2
ryom <ryouma7172z@gmail.com>
2026-01-31 00:17:06 +0900

    commit 1

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from car!")
+# main.py
 
+from dimod import BinaryQuadraticModel
+from dwave.system import DWaveSampler, EmbeddingComposite
 
-if __name__ == "__main__":
-    main()
+# Define the problem (simple Ising model)
+# H = -J_12 * s_1 * s_2 - h_1 * s_1 - h_2 * s_2
+# J_12 = -1, h_1 = 0, h_2 = 0
+
+bqm = BinaryQuadraticModel({0: 0.0, 1: 0.0}, {(0, 1): -1.0}, 0.0, 'SPIN')
+
+# Set up the D-Wave sampler
+# You will need to have your D-Wave API token configured
+# For local testing, you can use a classical sampler like ExactSolver
+sampler = EmbeddingComposite(DWaveSampler())
+
+# Run the problem on the sampler
+sampleset = sampler.sample(bqm, num_reads=10)
+
+# Print the results
+print(sampleset.first.sample)
+print(sampleset.first.energy)
