4318926b4bfbc921779a0864c1c8bd4ce3fa6043
Ryoma Morita <ryouma7172z@gmail.com>
2026-04-23 21:02:37 +0900

    fix: Remove redundant dropna() and handle NaN origins in coordinates

--- otss1.py
+++ otss1.py
@@ -19,8 +19,6 @@ for i, r in enumerate(origins_and_destinations):
     else:
         df.loc[i] = [np.nan, r[0]]
 
-df = df.dropna()
-
 coords = {
     "野田店": np.array([34.654796952803004, 133.90363607639333]),
     "高野店": np.array([35.07383804491031, 134.0504428999516]),
@@ -39,7 +37,22 @@ coords = {
     "津山B": np.array([35.05537760000001, 133.99844677116405]),
     "真庭B": np.array([35.00940701246312, 133.73504602883594]),
     "PDI": np.array([34.737335733341055, 134.01831081349152]),
-    "営業所": np.array([34.622486758516715, 133.80092669685916])
+    "営業所": np.array([34.622486758516715, 133.80092669685916]),
+    "○○水島店": np.array([34.52978972589754, 133.75017584976348]),
+    "○○泉田": np.array([34.63200770824816, 133.92227706152727]),
+    "○○岡山": np.array([34.628346669104545, 133.87781675432046]),
+    "○○東岡山": np.array([34.67051745172791, 133.9734545314257]),
+    "○○オート": np.array([34.927103787508734, 133.49877569916796]),
+    "○○オートセンター": np.array([34.55142595564274, 133.87368200370835]),
+    "○○会社": np.array([34.47946213728613, 133.8019328807972]),
+    "○○倉敷": np.array([34.586288134222016, 133.7966207167915]),
+    "○○岡山青江店": np.array([34.52978972589754, 133.75017584976348]),
+    "○○車体": np.array([34.6362446037451, 133.9263989083228]),
+    "○○車輛": np.array([34.68094485197269, 133.99488344034324]),
+    "△△車輛": np.array([34.71033749824907, 133.86233713956912]),
+    "○○岡山営業所": np.array([34.6199524505259, 133.87903214522797]),
+    "○○岡山新見店": np.array([34.994260979999055, 133.44116203495304]),
+    "○○岡山津山店": np.array([35.05477433641165, 134.00309984286147])
 }
 
 def eauclid_norm(point1, point2):
@@ -53,10 +66,12 @@ linear_terms = {i: lam_load_cap * (1 - 2 * load_capacity) for i in range(N_DATA)
 quadratic_terms = {(i, j): 2 * lam_load_cap for i in range(N_DATA) for j in range(i + 1, N_DATA)}
 
 for i in range(N_DATA):
-    origin_i = coords.get(df['origin'].iloc[i], np.zeros(2))
+    o_i = df['origin'].iloc[i]
+    origin_i = coords.get(o_i, np.zeros(2)) if pd.notna(o_i) else coords['PDI']
     destination_i = coords.get(df['destination'].iloc[i], np.zeros(2))
     for j in range(i + 1, N_DATA):
-        origin_j = coords.get(df['origin'].iloc[j], np.zeros(2))
+        o_j = df['origin'].iloc[j]
+        origin_j = coords.get(o_j, np.zeros(2)) if pd.notna(o_j) else coords['PDI']
         destination_j = coords.get(df['destination'].iloc[j], np.zeros(2))
         
         # Example QUBO term (replace with actual logic)
